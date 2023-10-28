f5b5d7b83779bbfda0b42d0fd9359c51b3cf22ad
DanyPlaya <danila.lusuy@gmail.com>
2023-10-28 18:00:44 +0300

    feat: create base AppProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div>hello</div>;
-}
-
-export default App;
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./providers/routeProvider";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "@/shared/lib/queryClient";
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <AppRouter />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
