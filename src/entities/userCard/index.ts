12d172ddbcfe1c2ec3f599f2dbc3ba5397ac50fa
DanyPlaya <danila.lusuy@gmail.com>
2023-10-29 14:56:53 +0300

    fix: change file name

--- src/entities/userCard/index.ts
+++ src/entities/userCard/index.ts
@@ -1,1 +1,4 @@
-export { UserCard } from './ui/userCard'
+export { UserCard } from './ui/userCard/userCard'
+export { useGetUserById, useGetUserByName } from './api/userCardApi'
+export { useUserStore } from './model/userStore'
+export type { UserData } from './types/types'
