82ea9908d294e666b8fac66716b1c244bcac2944
DanyPlaya <danila.lusuy@gmail.com>
2023-10-29 15:03:07 +0300

    feat: decompose MainPage component

--- src/pages/main/ui/mainPage.tsx
+++ src/pages/main/ui/mainPage.tsx
@@ -1,12 +1,10 @@
+import { UserCardList } from "@/widgets/userCard";
 import { Box } from "@chakra-ui/react";
 import cls from "./mainPage.module.css";
-import { UserCard } from "@/entities/userCard/ui/userCard";
-import { PostSearchBar } from "@/features/postFilter";
 export const MainPage = () => {
   return (
-    <Box className={cls.main}>
-      <PostSearchBar />
-      <UserCard phone="234234" email="234234234" name="asdrfasdf" />
+    <Box className={cls.main} overflowX={"hidden"}>
+      <UserCardList />
     </Box>
   );
 };
