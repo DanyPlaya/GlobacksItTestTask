29d30e30cd26001b67733e09345885648d335c31
DanyPlaya <danila.lusuy@gmail.com>
2023-10-29 15:00:09 +0300

    feat: add control input and add types for my component

--- src/features/postFilter/ui/postFilter.tsx
+++ src/features/postFilter/ui/postFilter.tsx
@@ -1,10 +1,21 @@
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
-export const PostSearchBar = () => {
+import { ChangeEvent } from "react";
+type PostSearchBarType = {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+export const PostSearchBar = (props: PostSearchBarType) => {
+  const { onChange, value } = props;
   return (
     <InputGroup width="1120px" height="48px" marginBottom={"32px"}>
       <InputRightElement children={<SearchIcon />} />
-      <Input border="1px solid #D4DEFE" borderRadius="24px" />
+      <Input
+        value={value}
+        onChange={(e) => onChange(e)}
+        border="1px solid #D4DEFE"
+        borderRadius="24px"
+      />
     </InputGroup>
   );
 };
